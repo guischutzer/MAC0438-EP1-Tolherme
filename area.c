524171f42076db5f0bb7e27b3f9aa0f72e623045
Guilherme Schützer <guischutzer@gmail.com>
2016-03-23 16:30:51 -0300

    area.c correto

--- area.c
+++ area.c
@@ -3,25 +3,30 @@
 #include <time.h>
 #include <math.h>
 
-double area(int r){
-  int i = 0;
+double area(int s){
+  int i = 0, t = 0;
   double xs, ys;
   time_t T;
 
   srand((unsigned) time(&T));
 
-  printf("Faremos %d iteracoes.\n", r);
-  for(i = 0; i < r; i++){
+  printf("Faremos %d iteracoes.\n", s);
+  for(i = 0; i < s; i++){
     xs = (double)rand() / (double)RAND_MAX;
     ys = (double)rand() / (double)RAND_MAX;
     ys = ys/cos(1);
-    printf("%8f, %8f\n", xs, ys);
+    printf("x = %8f, y = %8f, 1/cos(x) = %8f\n", xs, ys, 1/cos(xs));
+    if(1/cos(xs) >= ys)
+      t++;
   }
 
-  return 0;
+  printf("t = %d\n", t);
+  printf("s = %d\n", s);
+
+  return (double) t/(double) s/cos(1);
 }
 
 int main(int argc, char *argv[]){
   double a = area(atoi(argv[1]));
-  printf("Área estimada: %f.\n", a);
+  printf("Area estimada: %f.\n", a);
 }
